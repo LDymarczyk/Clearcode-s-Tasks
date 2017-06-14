71ce6aad6b03d3a50fafa5bd1129712e8adf77fd
LDymarczyk <laura.dymarczyk@smcebi.edu.pl>
2017-06-14 15:27:32 +0200

    Dodanie warunkow doliczania punktow obrazen

--- task2.py
+++ task2.py
@@ -2,16 +2,21 @@ grimoire={'fe':1,'je':2,'jee':3,'ain':3,'dai':5,'ne':2,'ai':2}
 subspells=grimoire.values()
 
 def count_dmg(spell):
-	"""Counts what damage spell deal"""
-	dmg=0
-	
-	i=0
-	while i<len(spell)-2:
-	    if spell[i:i+1] in subspell:
-		    dmg+=grimoire[spell[i:i+1]]
-			i+=2
-		
-	return dmg
+    """Counts what damage spell deal"""
+    dmg, bad_one, i = 0, 0, 0
+    while i<len(spell)-2:
+	print spell[i:i+2] == 'fe'
+        if spell[i:i+2] in subspells:
+            dmg+=grimoire[spell[i:i+2]]
+            i+=2
+        elif spell[i:i+3] in subspells:
+            dmg+=grimoire[spell[i:i+3]]
+            i+=3
+        else:
+            bad_one+=1
+            i+=1
+	print dmg-bad_one
+    return dmg-bad_one
 
 def check_feai(spell):
     """Checks if there is spell in spell"""
@@ -21,7 +26,7 @@ def check_feai(spell):
         if letter=='f' and spell[i+1]=='e': start=i
         elif letter=='a' and spell[i+1]=='i': end=i
     if start<end: return spell[start+2:end]
-	return None
+    return None
 
 def damage(spell):
     """
@@ -31,9 +36,9 @@ def damage(spell):
     :return: points of damage
     """
     dmg=0
-	if "fe" in spell and "ai" in spell:
-	    spell=check_feai(spell)
-		if spell!=None:
-		    dmg+=3
-			dmg+=count_dmg(spell)
+    if "fe" in spell and "ai" in spell:
+        spell=check_feai(spell)
+        if spell!=None:
+            dmg+=3
+            dmg+=count_dmg(spell)
     return dmg
