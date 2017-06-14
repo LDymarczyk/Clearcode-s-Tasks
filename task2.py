6d0c27f8780959ab9c7bb53561d0c9b044eef275
LDymarczyk <laura.dymarczyk@smcebi.edu.pl>
2017-06-14 14:22:32 +0200

    Rozpoczecie pracy nad funkcja count_dmg()

--- task2.py
+++ task2.py
@@ -1,3 +1,18 @@
+grimoire={'fe':1,'je':2,'jee':3,'ain':3,'dai':5,'ne':2,'ai':2}
+subspells=grimoire.values()
+
+def count_dmg(spell):
+	"""Counts what damage spell deal"""
+	dmg=0
+	
+	i=0
+	while i<len(spell)-2:
+	    if spell[i:i+1] in subspell:
+		    dmg+=grimoire[spell[i:i+1]]
+			i+=2
+		
+	return dmg
+
 def check_feai(spell):
     """Checks if there is spell in spell"""
     start, end = 0, 0
@@ -5,7 +20,8 @@ def check_feai(spell):
         letter=spell[i]
         if letter=='f' and spell[i+1]=='e': start=i
         elif letter=='a' and spell[i+1]=='i': end=i
-    return spell[start+2:end]
+    if start<end: return spell[start+2:end]
+	return None
 
 def damage(spell):
     """
@@ -14,8 +30,10 @@ def damage(spell):
     :rtype: int
     :return: points of damage
     """
-    grimoire={1:'dai',2:'ain',3:'ai',4:'jee',5:'je',6:'fe',7:'ne'}
-    subspell_d={'fe':1,'je':2,'jee':3,'ain':3,'dai':5,'ne':2,'ai':2}
-    subspells=subspell_d.values()
     dmg=0
+	if "fe" in spell and "ai" in spell:
+	    spell=check_feai(spell)
+		if spell!=None:
+		    dmg+=3
+			dmg+=count_dmg(spell)
     return dmg
