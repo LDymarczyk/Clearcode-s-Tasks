a93080d5072845c6db7b7495b6818737fb9baabc
LDymarczyk <laura.dymarczyk@smcebi.edu.pl>
2017-06-17 01:52:02 +0200

    dodanie zmiennej dlugosci interesujacych nas danych + rozroznienie miesiecy od lat

--- task1.py
+++ task1.py
@@ -2,5 +2,9 @@ def group_by(stream,sort_type):
     """Input: stream (file), sort_type == ['year','month']
     Output: dictionary {years/months : int}"""
     stream_lines=stream.readlines()
-    date_start=stream_lines[0].find("Launch Date")
-	return date_start
+    start_point=stream_lines[0].find("Launch Date")
+	length=4
+	if sort_type=="month":
+		start_point+=5
+		length=3
+    return start_point
