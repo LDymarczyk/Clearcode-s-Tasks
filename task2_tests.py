34fbdb5bff3c3a939da198e1e7f41e22191d4754
LDymarczyk <laura.dymarczyk@smcebi.edu.pl>
2017-06-16 12:29:24 +0200

    plik task2_tests - zmiana funkcji count_dmg1 na count_dmg

--- task2_tests.py
+++ task2_tests.py
@@ -1,10 +1,10 @@
 from task2 import damage
-from task2 import count_dmg1
+from task2 import count_dmg
 from task2 import check_feai as check
 
 #print check('feeai')
 assert check('fdafafeajain') == 'aj'
-assert count_dmg1('feeai') == 2
+assert count_dmg('feeai') == -1
 
 assert damage('feeai') == 2
 assert damage('feaineain') == 1 + 2 + 2 + 2 == 7 ##(fe-ai-ne-ai) - not (fe-ain-ai) because 1+3+2 = 6
