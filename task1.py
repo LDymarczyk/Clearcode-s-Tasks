e1f3d2118b492110c1389024bfbb971f8300b006
LDymarczyk <laura.dymarczyk@smcebi.edu.pl>
2017-06-18 00:54:44 +0200

    Wprowadzenie koniecznych poprawek po tescie (niwelujacych index error oraz bledy skladniowe), program nie liczy wielu linii

--- task1.py
+++ task1.py
@@ -2,21 +2,23 @@ def create_dict(stream_lines,start_point,length,success,suc_pointer):
     """Creating dictionary {years/months : int}"""
     dict={}
     for line in stream_lines:
-        key,suc=line[start_point:start_point+length],line[suc_pointer]
-        if key!='    ' and key!='   ' and success in [suc, None]:
-            keys=dict.keys()
-            if key in keys:
-                dict[key]+=1
-            else:
-                dict[key]=1
+        if len(line)>=suc_pointer:
+            key,suc=line[start_point:start_point+length],line[suc_pointer]
+            if success in [suc, None]:#key!='    ' and key!='   ' and success in [suc, None]:
+                keys=dict.keys()
+                if key in keys:
+                    dict[key]+=1
+                else:
+                    dict[key]=1
     return dict
 
-def group_by(stream,field,succes):
+def group_by(stream,field,success=None):
     """Input: stream (file), field == ['year','month']
     Output: dictionary {years/months : int}"""
     stream_lines=stream.readlines()
     start_point,suc_pointer=stream_lines[0].find("Launch Date"),stream_lines[0].find("Suc")
     length=4
+    print suc_pointer, stream_lines[0], len(stream_lines[2])
     if field=="month":
         start_point+=5
         length=3
