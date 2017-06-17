174aa69b1e0ff2a40c2e9450c253747bf03fb167
LDymarczyk <laura.dymarczyk@smcebi.edu.pl>
2017-06-18 00:09:05 +0200

    Dodanie zmiennej suc_pointer w pliku task1.py

--- task1.py
+++ task1.py
@@ -1,4 +1,4 @@
-def create_dict(stream_lines,start_point,length,success):
+def create_dict(stream_lines,start_point,length,success,suc_pointer):
     """Creating dictionary {years/months : int}"""
     dict={}
     for line in stream_lines:
@@ -15,9 +15,9 @@ def group_by(stream,field,succes):
     """Input: stream (file), field == ['year','month']
     Output: dictionary {years/months : int}"""
     stream_lines=stream.readlines()
-    start_point=stream_lines[0].find("Launch Date")
+    start_point,suc_pointer=stream_lines[0].find("Launch Date"),stream_lines[0].find("Suc")
     length=4
     if field=="month":
         start_point+=5
         length=3
-    return create_dict(stream_lines[2:],start_point,length,success)
+    return create_dict(stream_lines[2:],start_point,length,success,suc_pointer)
