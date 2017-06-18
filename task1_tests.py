dab6a174feae15a7e455caa87f89297e82f0947c
LDymarczyk <laura.dymarczyk@smcebi.edu.pl>
2017-06-18 14:32:04 +0200

    Dodanie testow do task1_test.py

--- task1_tests.py
+++ task1_tests.py
@@ -1,4 +1,10 @@
 from task1 import group_by
 
+print "test with year, filter: None"
 print group_by(open("launchlog.txt"),'year')
-print group_by(open("launchlog.txt"),'month',False)
+print "test with month, filter: None"
+print group_by(open("launchlog.txt"),'month')
+print "test with month, filter: failure"
+print group_by(open("launchlog.txt"),'year',False)
+print "test with month, filter: Succed"
+print group_by(open("launchlog.txt"),'year',True)
