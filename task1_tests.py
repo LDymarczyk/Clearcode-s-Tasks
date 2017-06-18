8c34b4ec8bafaabcbb7d5969ddb76c4fb2f7d2b3
LDymarczyk <laura.dymarczyk@smcebi.edu.pl>
2017-06-18 22:28:39 +0200

    Poprawienie opisow testow w task1_tests.py

--- task1_tests.py
+++ task1_tests.py
@@ -4,9 +4,9 @@ print "test with year, filter: None"
 print group_by(open("launchlog.txt"),'year')
 print "test with month, filter: None"
 print group_by(open("launchlog.txt"),'month')
-print "test with month, filter: failure"
+print "test with year, filter: failure"
 print group_by(open("launchlog.txt"),'year',False)
-print "test with month, filter: Succed"
+print "test with year, filter: Succed"
 print group_by(open("launchlog.txt"),'year',True)
 print "test with bad argument"
 print group_by(open("launchlog.txt"),'day',True)
