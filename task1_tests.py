91af59da0866a191fcecd256bf506e7a010203bb
LDymarczyk <laura.dymarczyk@smcebi.edu.pl>
2017-06-18 22:35:52 +0200

    zakomentowanie testow z printem w pliku task1_tests.py

--- task1_tests.py
+++ task1_tests.py
@@ -1,15 +1,15 @@
 from task1 import group_by
 
-print "test with year, filter: None"
-print group_by(open("launchlog.txt"),'year')
-print "test with month, filter: None"
-print group_by(open("launchlog.txt"),'month')
-print "test with year, filter: failure"
-print group_by(open("launchlog.txt"),'year',False)
-print "test with year, filter: Succed"
-print group_by(open("launchlog.txt"),'year',True)
-print "test with bad argument"
-print group_by(open("launchlog.txt"),'day',True)
+#print "test with year, filter: None"
+#print group_by(open("launchlog.txt"),'year')
+#print "test with month, filter: None"
+#print group_by(open("launchlog.txt"),'month')
+#print "test with year, filter: failure"
+#print group_by(open("launchlog.txt"),'year',False)
+#print "test with year, filter: Succed"
+#print group_by(open("launchlog.txt"),'year',True)
+#print "test with bad argument"
+#print group_by(open("launchlog.txt"),'day',True)
 
 assert group_by(open("launchlog.txt"),'year',True)['2016']+group_by(open("launchlog.txt"),'year',False)['2016']==group_by(open("launchlog.txt"),'year')['2016']
 assert group_by(open("launchlog.txt"),'year',True)['2010']+group_by(open("launchlog.txt"),'year',False)['2010']==group_by(open("launchlog.txt"),'year')['2010']
