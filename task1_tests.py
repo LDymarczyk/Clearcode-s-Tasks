b151249116b4b9703c92b3c59d67543fd3caf78d
LDymarczyk <laura.dymarczyk@smcebi.edu.pl>
2017-06-18 22:34:53 +0200

    Dodanie testow type assert do pliku task1_tests.py

--- task1_tests.py
+++ task1_tests.py
@@ -10,3 +10,12 @@ print "test with year, filter: Succed"
 print group_by(open("launchlog.txt"),'year',True)
 print "test with bad argument"
 print group_by(open("launchlog.txt"),'day',True)
+
+assert group_by(open("launchlog.txt"),'year',True)['2016']+group_by(open("launchlog.txt"),'year',False)['2016']==group_by(open("launchlog.txt"),'year')['2016']
+assert group_by(open("launchlog.txt"),'year',True)['2010']+group_by(open("launchlog.txt"),'year',False)['2010']==group_by(open("launchlog.txt"),'year')['2010']
+assert group_by(open("launchlog.txt"),'year')['1957']==4
+assert group_by(open("launchlog.txt"),'year')['1958']==28
+assert group_by(open("launchlog.txt"),'year')['1959']==31
+assert group_by(open("launchlog.txt"),'year')['1960']==66
+
+assert group_by(open("launchlog.txt"),'year',True)['1958']==8 #I count it by myself
